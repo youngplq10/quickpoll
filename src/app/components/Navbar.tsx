a09fb0ef24e7f7260fd0f16a01a009efae3377da
starzynski5_ <patryk.plq@o2.pl>
2024-12-13 11:32:11 +0100

    few changes

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const Navbar = () => {
+    return(
+        <>
+            <Box sx={{ flexGrow: 1 }}>
+            <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            MUI
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <Button sx={{ color: '#fff' }}>
+                a
+              </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+            </Box>
+        </>
+    )
+}
+
+export default Navbar
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/app/components/Navbar";
+
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
